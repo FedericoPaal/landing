4406febaaa2d124e6592ab3fe5284525a4435507
Fede Paal <federicopaal@gmail.com>
2025-07-23 16:01:18 -0300

    Se mejora estetica de las card respecto a su contenido para mayor acierto a lo requerido

--- src/components/ProyectoClientes.tsx
+++ src/components/ProyectoClientes.tsx
@@ -29,7 +29,7 @@ export default function ProyClientes(){
                         />
                         <div className='text-clientes'>
                             <h4 className="text-xl font-bold mt-4">{proyecto.title}</h4>
-                            <p className="text-m">{proyecto.description}</p>
+                            <p className="text-md">{proyecto.description}</p>
                             <button className="mt-4 px-6 py-2 bg-primary text-white rounded-lg hover:bg-primary-hover transition-colors button-clientes">
                                 Ver
                             </button>
